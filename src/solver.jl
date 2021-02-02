cf2f69b20527158d6e7abc5cf8b6540c2804383b
Tomas Omasta <omastto1@fel.cvut.cz>
2021-02-02 22:18:02 +0100

    Updated methods to work with FHWwapper

--- src/solver.jl
+++ src/solver.jl
@@ -9,22 +9,22 @@
 
 
 # Policy struct created according to one used in DiscreteValueIteration
-mutable struct FiniteHorizonValuePolicy{Q<:AbstractArray, U<:AbstractMatrix, P<:AbstractMatrix, A, M<:MDP} <: Policy
+mutable struct FiniteHorizonValuePolicy{Q<:AbstractArray, U<:AbstractMatrix, P<:AbstractMatrix, A, W<:FHWrapper} <: Policy
     qmat::Q
     util::U 
     policy::P 
     action_map::Vector{A}
     include_Q::Bool 
-    mdp::M
+    w::W
 end
 
 # Policy constructor
-function FiniteHorizonValuePolicy(mdp::MDP)
-    return FiniteHorizonValuePolicy(zeros(mdp.horizon + 1, mdp.no_states, length(mdp.actions)), zeros(mdp.horizon + 1, mdp.no_states), ones(Int64, mdp.horizon + 1, mdp.no_states), ordered_actions(mdp), true, mdp)
+function FiniteHorizonValuePolicy(w::FHWrapper)
+    return FiniteHorizonValuePolicy(zeros(w.horizon + 1, length(states(w.m)), length(actions(w.m))), zeros(w.horizon + 1, length(states(w.m))), ones(Int64, w.horizon + 1, length(states(w.m))), ordered_actions(w.m), true, w)
 end
 
 function action(policy::FiniteHorizonValuePolicy, s::S) where S
-    sidx = stage_stateindex(policy.mdp, s, s.epoch)
+    sidx = stage_stateindex(policy.w.m, s, s.epoch)
     aidx = policy.policy'[sidx]
     return policy.action_map[aidx]
 end
@@ -38,17 +38,17 @@ function addepochrecord(fhpolicy::FiniteHorizonValuePolicy, qmat, util, policy, 
 end
 
 # MDP given horizon 5 assumes that agent can move 4 times
-function solve(mdp::MDP; verbose::Bool=false, new_VI::Bool=true)
-    fhpolicy = FiniteHorizonValuePolicy(mdp)
-    util = fill(0., mdp.no_states) # XXX not all MDPs have a no_states field. Suggest using length(states(mdp))
+function solve(w::FHWrapper; verbose::Bool=false, new_VI::Bool=true)
+    fhpolicy = FiniteHorizonValuePolicy(w)
+    util = fill(0., length(states(w.m)))
 
-    for stage=mdp.horizon:-1:1
+    for stage=w.horizon:-1:1
 
         if verbose
             println("EPOCH: $stage")
         end
 
-        stage_q, util, pol = valueiterationsolver(mdp, stage, util)
+        stage_q, util, pol = valueiterationsolver(w, stage, util)
 
         fhpolicy = addepochrecord(fhpolicy, stage_q, util, pol, stage)
 
