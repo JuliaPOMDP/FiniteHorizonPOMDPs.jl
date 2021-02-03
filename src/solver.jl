7aa0039df2114eafc5efcf7686bf3c60fa316375
Tomas Omasta <omastto1@fel.cvut.cz>
2021-02-04 00:25:03 +0100

    Fixed merge errors

--- src/solver.jl
+++ src/solver.jl
@@ -10,18 +10,18 @@
 
 
 # Policy struct created according to one used in DiscreteValueIteration
-mutable struct FiniteHorizonValuePolicy{Q<:AbstractArray, U<:AbstractMatrix, P<:AbstractMatrix, A, M<:MDP} <: Policy
+mutable struct FiniteHorizonValuePolicy{Q<:AbstractArray, U<:AbstractMatrix, P<:AbstractMatrix, A, W<:FHWrapper} <: Policy
     qmat::Q
     util::U 
     policy::P 
     action_map::Vector{A}
     include_Q::Bool 
-    mdp::M
+    w::W
 end
 
 # Policy constructor
-function FiniteHorizonValuePolicy(mdp::MDP)
-    return FiniteHorizonValuePolicy(zeros(mdp.horizon, mdp.no_states, length(mdp.actions)), zeros(mdp.horizon, mdp.no_states), ones(Int64, mdp.horizon, mdp.no_states), ordered_actions(mdp), true, mdp)
+function FiniteHorizonValuePolicy(w::FHWrapper)
+    return FiniteHorizonValuePolicy(zeros(w.horizon + 1, length(states(w.m)), length(actions(w.m))), zeros(w.horizon + 1, length(states(w.m))), ones(Int64, w.horizon + 1, length(states(w.m))), ordered_actions(w.m), true, w)
 end
 
 function action(policy::FiniteHorizonValuePolicy, s::S) where S
@@ -48,25 +48,17 @@ function stage_states(mdp::MDP, epoch::Int64) end
 
 function stage_stateindex(mdp::MDP, s, epoch::Int64) end
 
-# Global variable for storing number of epoch in order to pass it to functions from outer solvers
-# Is there a better way to achieve this?
-fhepoch = -1
-
-
-# Is it possible to change name of this function to solve?
-# The problem is that I am not able to use for example DiscreteValueIteration.solve() as I do not know the Solver in advance
-
-# MDP given horizon 5 assumes that agent can move 4 times
-function solve(mdp::MDP; verbose::Bool=false, new_VI::Bool=true)
-    fhpolicy = FiniteHorizonValuePolicy(mdp)
-    util = fill(0., mdp.no_states)
+# MDP given horizon 5 assumes that agent can move 5 times
+function solve(w::FHWrapper; verbose::Bool=false, new_VI::Bool=true)
+    fhpolicy = FiniteHorizonValuePolicy(w)
+    util = fill(0., length(states(w.m)))
 
     for stage=w.horizon:-1:1
         if verbose
             println("Stage: $stage")
         end
 
-        stage_q, util, pol = valueiterationsolver(mdp, epoch, util)
+        stage_q, util, pol = valueiterationsolver(w, stage, util)
 
         fhpolicy = addstagerecord(fhpolicy, stage_q, util, pol, stage)
 
@@ -78,12 +70,6 @@ function solve(mdp::MDP; verbose::Bool=false, new_VI::Bool=true)
             println(util)
             println("policy")
             println(policy)
-            # println("action_map")
-            # println(policy.action_map)
-            # println("include_Q")
-            # println(policy.include_Q)
-            # println("mdp")
-            # println(policy.mdp)
             println("\n\n\n")
         end
     end
